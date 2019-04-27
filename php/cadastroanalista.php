f1ebb008d6ac86fd57c67299dce7605c0504cd77
Erick Johnson Menezes <erick123j@hotmail.com>
2019-04-26 22:29:15 -0300

    corrigido bug cadastro formulario, trocado get por post

--- php/cadastroanalista.php
+++ php/cadastroanalista.php
@@ -4,22 +4,22 @@ session_start();
 
 if($_SESSION['auth'] == 'true'){
     
-    if(isset($_GET['nome']) && isset($_GET['login']) && isset($_GET['senha']) && isset($_GET['status']) && isset($_GET['email']) && isset($_GET['cargo'])){
+    if(isset($_POST['nome']) && isset($_POST['login']) && isset($_POST['senha']) && isset($_POST['status']) && isset($_POST['email']) && isset($_POST['cargo'])){
         
         include "UsuarioDAO.php";
 
-        $nome = $_GET['nome'];
-        $login = $_GET['login'];
-        $senha = $_GET['senha'];
-        $email = $_GET['email'];
-        $cargo = $_GET['cargo'];
-        $status = $_GET['status'];
+        $nome = $_POST['nome'];
+        $login = $_POST['login'];
+        $senha = $_POST['senha'];
+        $email = $_POST['email'];
+        $cargo = $_POST['cargo'];
+        $status = $_POST['status'];
 
         $resultado = UsuarioDAO::create($nome, $login, $senha, $email, $status, $cargo); 
         if($resultado == 1){
             echo '<div class="alert alert-success" role="alert">Usuário cadastrado com sucesso!</div>';
         } else {
-            echo '<div class="alert alert-danger" role="alert">Erro ao cadastrar usuário!</div>';
+            echo '<div class="alert alert-danger" role="alert">Erro ao cadastrar usuário.</div>';
         }
         
         
