3dc145d42eeefd650aa59ceb777c46bf939ca2f2
ErickJMenezes <erick123j@hotmail.com>
2019-05-13 18:35:15 -0300

    Adicionado linha de comentário

--- php/cadastrochamado.php
+++ php/cadastrochamado.php
@@ -1,4 +1,5 @@
 <?php
+#Inicio da seção
 session_start();
 
 include 'lib/UsuarioDAO.php';
@@ -7,12 +8,12 @@ include 'lib/ChamadoDAO.php';
 
 //Verifica se o usuário está autenticado
 if($_SESSION['auth'] == 'true'){
-    
-    
+
+
     //Verifica se foram inseridos informações no formulário da página html
     if(isset($_POST['titulo']) && isset($_POST['descricao']) && isset($_POST['analista']) && isset($_POST['requerente']) && isset($_POST['ativo'])){
-        
-        
+
+
         //Pega as informações do formulário
         $nome = $_POST['titulo'];
         $descricao = $_POST['descricao'];
@@ -23,21 +24,21 @@ if($_SESSION['auth'] == 'true'){
 
         // Cadastra o chamado no banco de dados
         $chamado = ChamadoDAO::create($nome, $status, $requerente, $_GET['id'], $ativo);
-        
+
         //Verifica se foi cadastrado com sucesso
         if($chamado == 1){
             //header('Location: cadastrodechamados.php?e=0');  // mensagem de sucesso
         } else {
             //header('Location: cadastrodechamados.php?e=2'); //mensagem de erro
         }
-        
+
     } else {
-        
+
         //header('Location: cadastrodechamados.php?e=1');
-        
+
     }
-    
-    
+
+
 // Caso o usuário não esteja autenticado, o sistema o mandará para a página de login.
 }else{
     header('Location: ../paginas/index.php?e=4');
