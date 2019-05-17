a79ad4963bb35f3554e11591138758d5040c9996
ErickJMenezes <erick123j@hotmail.com>
2019-05-16 22:55:27 -0300

    Adicionado comentários

--- paginas/analista/home.php
+++ paginas/analista/home.php
@@ -1,3 +1,23 @@
+<?php
+session_start();
+/*
+    Verifica se ja existe usuário logado, caso sim verifica o cargo e redireciona para a pagina correta.
+    */
+    if(isset($_SESSION['auth'])){
+        if($_SESSION['auth'] == 'true'){ //verifica se o usuário está autenticado
+            include '../../php/lib/UsuarioDAO.php';
+            $usuario = new UsuarioDAO($_SESSION['id']);
+            if($usuario->cargo == 'administrador'){
+              //Manda para a home da sua página
+              header('Location: ../analista/home.php');
+            } else if($usuario->cargo == 'analista'){
+              //Não faz nada
+            }
+        } else {
+            header('Location: ../index.php');
+        }
+    }
+?>
 <!DOCTYPE html>
 <html lang="pt" dir="ltr">
   <head>
@@ -7,7 +27,7 @@
     <link rel="stylesheet" href="../../css/estilo.css" media="screen">
   </head>
   <body>
-    
+
     <script type="text/javascript" src="../../js/bootstrap.js"></script>
     <script type="text/javascript" src="../../js/jquery.js"></script>
   </body>
